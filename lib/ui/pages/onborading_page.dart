fdca0248f751a51fb8d531ffbd327f68e84503a2
faricandrevano <faric26andrevano@gmail.com>
2024-06-20 22:40:55 +0700

    modified onboarding

--- lib/ui/pages/onborading_page.dart
+++ lib/ui/pages/onborading_page.dart
@@ -7,7 +7,7 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> imageCarousel = [
+    final List<Widget> imageCarousel = [
       Image.asset(
         'assets/img_onboarding1.png',
         height: 331,
@@ -33,6 +33,90 @@ class OnboardingPage extends StatelessWidget {
                 height: 331,
                 viewportFraction: 1,
                 enableInfiniteScroll: false,
+              ),
+            ),
+            const SizedBox(height: 80),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 22),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Grow Your \nFinancial Today',
+                    textAlign: TextAlign.center,
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  const SizedBox(height: 26),
+                  Text(
+                    'Our system is helping you to \nachieve a better goal',
+                    textAlign: TextAlign.center,
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: regular,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: blueColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: lightBackgroundColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: lightBackgroundColor,
+                        ),
+                      ),
+                      const Spacer(),
+                      // ignore: sized_box_for_whitespace
+                      Container(
+                        width: 150,
+                        height: 50,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            backgroundColor: purpleColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(56),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Continue',
+                            style:
+                                whiteTextStyle.copyWith(fontWeight: semiBold),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             )
           ],
